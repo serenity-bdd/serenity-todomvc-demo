88832c6e5879d37a37ce8f683f0e00eb3bcedd71
John Ferguson Smart <john.smart@wakaleo.com>
2026-02-24 12:28:17 +0000

    Updated to 5.3.0

--- src/test/java/net/serenitybdd/demos/todos/cucumber/CucumberTestSuite.java
+++ src/test/java/net/serenitybdd/demos/todos/cucumber/CucumberTestSuite.java
@@ -1,18 +1,15 @@
 package net.serenitybdd.demos.todos.cucumber;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.*;
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("/features")
-//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@current")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
-        value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:target/test-results/timeline")
+@SelectPackages("todomvc.cucumber")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "net.serenitybdd.cucumber.core.plugin.SerenityReporterParallel,pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "net.serenitybdd.demos.todos.cucumber")
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
 public class CucumberTestSuite {
 }
--- src/test/resources/features/maintain_my_todo_list/deleting_todos.feature
+++ src/test/resources/features/maintain_my_todo_list/deleting_todos.feature
@@ -10,8 +10,3 @@ Feature: Deleting todos
     Given that Jane has a todo list containing Buy some milk, Walk the dog
     When she deletes the task called 'Walk the dog'
     Then her todo list should contain Buy some milk
-
-  Scenario: Delete all the items
-    Given that Jane has a todo list containing Buy some milk, Walk the dog
-    When she completes and deletes all the items
-    Then her todo list should be empty
